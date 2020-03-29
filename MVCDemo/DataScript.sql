7a5865806d5cec3982f7ad37bec66a6612c3854d
Kunal Shah <kkunalsshah@gmail.com>
2020-03-29 16:44:16 -0600

    Update data script

--- MVCDemo/DataScript.sql
+++ MVCDemo/DataScript.sql
@@ -9,3 +9,12 @@ VALUES
 
 GO 
 Select * From Category
+
+GO
+
+INSERT INTO [dbo].[Transaction]
+(Payee, Amount, PurchaseDate, Memo, CategoryId)
+VALUES
+('Payee 1', 225.24, '03/24/2020', 'Memo data test', 1),
+('Payee 2', 5840.56, '01/24/2020', 'Memo - free text area for any extra information.  Optional field ', 2),
+('Payee 3', 789.85, '06/24/2019', 'LiteracyPro Coding Challenge Project ', 2)
